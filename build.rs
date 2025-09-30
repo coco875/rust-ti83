e81fb4eb118282f80f66e58649a4533faf4be1bd
coco875 <pereira.jannin@gmail.com>
2025-09-30 23:40:40 +0200

    in theory fix print of error

--- build.rs
+++ build.rs
@@ -15,11 +15,11 @@ fn download_file(url: &str, output: &str) -> bool {
     match status {
         Ok(status) if status.success() => true,
         Ok(status) => {
-            eprintln!("Download failed with code: {}", status.code().unwrap_or(-1));
+            eprintln!("cargo:error=Download failed with code: {}", status.code().unwrap_or(-1));
             false
         }
         Err(e) => {
-            eprintln!("Error executing download command: {}", e);
+            eprintln!("cargo:error=Error executing download command: {}", e);
             false
         }
     }
@@ -31,52 +31,52 @@ fn download_cedev() {
         // should be improve to support windows
         if std::env::consts::OS == "linux" {
             if !download_file("https://github.com/CE-Programming/toolchain/releases/download/v13.0/CEdev-Linux.tar.gz", "CEdev-Linux.tar.gz") {
-                eprintln!("Failed to download CEdev");
+                eprintln!("cargo:error=Failed to download CEdev");
                 std::process::exit(1);
             }
             let status = std::process::Command::new("tar")
                 .args(&["-xzf", "CEdev-Linux.tar.gz"])
                 .status()
-                .expect("Failed to execute tar");
+                .expect("cargo:error=Failed to execute tar");
             if !status.success() {
-                eprintln!("Failed to extract CEdev");
+                eprintln!("cargo:error=Failed to extract CEdev");
                 std::process::exit(1);
             }
             let _ = std::fs::remove_file("CEdev-Linux.tar.gz");
         } else if std::env::consts::OS == "macos" {
             if std::env::consts::ARCH == "aarch64" {
                 if !download_file("https://github.com/CE-Programming/toolchain/releases/download/v13.0/CEdev-macOS-arm.dmg", "CEdev-macOS.dmg") {
-                    eprintln!("Failed to download CEdev");
+                    eprintln!("cargo:error=Failed to download CEdev");
                     std::process::exit(1);
                 }
             } else {
                 if !download_file("https://github.com/CE-Programming/toolchain/releases/download/v13.0/CEdev-macOS-intel.dmg", "CEdev-macOS.dmg") {
-                    eprintln!("Failed to download CEdev");
+                    eprintln!("cargo:error=Failed to download CEdev");
                     std::process::exit(1);
                 }
             }
             let status = std::process::Command::new("7z")
                 .args(&["x", "CEdev-macOS.dmg", "CE Programming Toolchain/CEdev/"])
                 .status()
-                .expect("Failed to execute 7z");
+                .expect("cargo:error=Failed to execute 7z");
             if !status.success() {
-                eprintln!("Failed to extract CEdev");
+                eprintln!("cargo:error=Failed to extract CEdev");
                 std::process::exit(1);
             }
             let status = std::process::Command::new("mv")
                 .args(&["CE Programming Toolchain/CEdev", "."])
                 .status()
-                .expect("Failed to execute mv");
+                .expect("cargo:error=Failed to execute mv");
             if !status.success() {
-                eprintln!("Failed to move CEdev");
+                eprintln!("cargo:error=Failed to move CEdev");
                 std::process::exit(1);
             }
             let status = std::process::Command::new("chmod")
                 .args(&["-R", "+x", "CEdev/bin"])
                 .status()
-                .expect("Failed to execute chmod");
+                .expect("cargo:error=Failed to execute chmod");
             if !status.success() {
-                eprintln!("Failed to chmod CEdev");
+                eprintln!("cargo:error=Failed to chmod CEdev");
                 std::process::exit(1);
             }
 
@@ -84,19 +84,19 @@ fn download_cedev() {
             let _ = std::fs::remove_dir_all("CE Programming Toolchain");
         } else if std::env::consts::OS == "windows" {
             if !download_file("https://github.com/CE-Programming/toolchain/releases/download/v13.0/CEdev-Windows.zip", "CEdev-Windows.zip") {
-                eprintln!("Failed to download CEdev");
+                eprintln!("cargo:error=Failed to download CEdev");
                 std::process::exit(1);
             }
             let status = std::process::Command::new("tar")
                 .args(&["-xf", "CEdev-Windows.zip"])
                 .status()
-                .expect("Failed to execute tar");
+                .expect("cargo:error=Failed to execute tar");
             if !status.success() {
-                eprintln!("Failed to extract CEdev");
+                eprintln!("cargo:error=Failed to extract CEdev");
                 std::process::exit(1);
             }
         } else {
-            eprintln!("CEdev not found and automatic download is only supported on Linux, Macos and Windows.");
+            eprintln!("cargo:error=CEdev not found and automatic download is only supported on Linux, Macos and Windows.");
             std::process::exit(1);
         }
     }
@@ -108,27 +108,27 @@ fn download_llvm_cbe() {
         // should be improve to support windows
         if std::env::consts::OS == "linux" {
             if !download_file("https://github.com/coco875/llvm-cbe/releases/download/0.0.1/llvm-cbe-linux-x64.zip", "llvm-cbe-ubuntu-latest-build.zip") {
-                eprintln!("Failed to download llvm-cbe");
+                eprintln!("cargo:error=Failed to download llvm-cbe");
                 std::process::exit(1);
             }
             let status = std::process::Command::new("unzip")
                 .args(&["-q", "llvm-cbe-ubuntu-latest-build.zip"])
                 .status()
-                .expect("Failed to execute unzip");
+                .expect("cargo:error=Failed to execute unzip");
             if !status.success() {
-                eprintln!("Failed to extract llvm-cbe");
+                eprintln!("cargo:error=Failed to extract llvm-cbe");
                 std::process::exit(1);
             }
             let _ = std::fs::remove_file("llvm-cbe-ubuntu-latest-build.zip");
         } else if std::env::consts::OS == "macos" {
             if std::env::consts::ARCH == "aarch64" {
                 if !download_file("https://github.com/coco875/llvm-cbe/releases/download/0.0.1/llvm-cbe-macos-arm.zip", "llvm-cbe-macos-build.zip") {
-                    eprintln!("Failed to download llvm-cbe");
+                    eprintln!("cargo:error=Failed to download llvm-cbe");
                     std::process::exit(1);
                 }
             } else {
                 if !download_file("https://github.com/coco875/llvm-cbe/releases/download/0.0.1/llvm-cbe-macos-intel.zip", "llvm-cbe-macos-build.zip") {
-                    eprintln!("Failed to download llvm-cbe");
+                    eprintln!("cargo:error=Failed to download llvm-cbe");
                     std::process::exit(1);
                 }
             }
@@ -136,28 +136,28 @@ fn download_llvm_cbe() {
             let status = std::process::Command::new("unzip")
                 .args(&["-q", "llvm-cbe-macos-build.zip"])
                 .status()
-                .expect("Failed to execute unzip");
+                .expect("cargo:error=Failed to execute unzip");
             if !status.success() {
-                eprintln!("Failed to extract llvm-cbe");
+                eprintln!("cargo:error=Failed to extract llvm-cbe");
                 std::process::exit(1);
             }
             let _ = std::fs::remove_file("llvm-cbe-macos-build.zip");
         } else if std::env::consts::OS == "windows" {
             if !download_file("https://github.com/coco875/llvm-cbe/releases/download/0.0.1/llvm-cbe-windows-x64.zip", "llvm-cbe-windows-build.zip") {
-                eprintln!("Failed to download llvm-cbe");
+                eprintln!("cargo:error=Failed to download llvm-cbe");
                 std::process::exit(1);
             }
             let status = std::process::Command::new("tar")
                 .args(&["-xf", "llvm-cbe-windows-build.zip"])
                 .status()
-                .expect("Failed to execute tar");
+                .expect("cargo:error=Failed to execute tar");
             if !status.success() {
-                eprintln!("Failed to extract llvm-cbe");
+                eprintln!("cargo:error=Failed to extract llvm-cbe");
                 std::process::exit(1);
             }
             let _ = std::fs::remove_file("llvm-cbe-windows-build.zip");
         } else {
-            eprintln!("llvm-cbe not found and automatic download is only supported on Linux and macOS ARM.");
+            eprintln!("cargo:error=llvm-cbe not found and automatic download is only supported on Linux and macOS ARM.");
             std::process::exit(1);
         }
     }
@@ -173,9 +173,9 @@ fn main() {
         let status = Command::new("rustc")
             .args(&["ti83-fake-linker.rs", "-o", "ti83-fake-linker"])
             .status()
-            .expect("Failed to execute rustc");
+            .expect("cargo:error=Failed to execute rustc");
         if !status.success() {
-            panic!("Failed to build ti83-fake-linker");
+            panic!("cargo:error=Failed to build ti83-fake-linker");
         }
         println!("cargo:rerun-if-changed=ti83-fake-linker.rs");
         println!("cargo:rustc-link-arg-bins=src/tice/wrapper.c");
